19c3381519e8e9827ec33c81fd05aec8dc0a5359
qnguyenhuy1999 <qnguyenhuy1999@gmail.com>
2020-07-27 14:09:00 +0700

    Fix show header & button active when type input

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import { ToastContainer } from "react-toastify";
@@ -9,48 +8,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import { configStore, history } from "./redux/configStore";
-import { HOME_ROUTES, AUTH_ROUTES } from "./constants/routes";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Loading from "./components/Loading";
 import { checkAuth } from "./actions/auth";
-import PrivateRoute from "./components/PrivateRoute";
-
-const showPrivateRoute = (routes) => {
-  let result = null;
-  if (routes.length > 0) {
-    result = routes.map((route, index) => {
-      return (
-        <PrivateRoute
-          key={index}
-          path={route.path}
-          name={route.name}
-          exact={route.exact}
-          component={route.component}
-        />
-      );
-    });
-  }
-  return result;
-};
-
-const showPublicRoute = (routes) => {
-  let result = null;
-  if (routes.length > 0) {
-    result = routes.map((route, index) => {
-      return (
-        <Route
-          key={index}
-          path={route.path}
-          name={route.name}
-          exact={route.exact}
-          component={route.component}
-        />
-      );
-    });
-  }
-  return result;
-};
+import Layout from "./pages/Layout";
 
 function App() {
   const store = configStore();
@@ -58,15 +17,7 @@ function App() {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Header />
-        <main>
-          <Switch>
-            {showPublicRoute(AUTH_ROUTES)}
-            {showPrivateRoute(HOME_ROUTES)}
-          </Switch>
-        </main>
-        <Footer />
-        <Loading />
+        <Layout />
       </ConnectedRouter>
       <ToastContainer />
     </Provider>
---
+++ client/src/pages/Layout/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { HOME_ROUTES, AUTH_ROUTES } from "../../constants/routes";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Loading from "../../components/Loading";
+import PrivateRoute from "../../components/PrivateRoute";
+
+const showPrivateRoute = (routes) => {
+  let result = null;
+  if (routes.length > 0) {
+    result = routes.map((route, index) => {
+      return (
+        <PrivateRoute
+          key={index}
+          path={route.path}
+          name={route.name}
+          exact={route.exact}
+          component={route.component}
+        />
+      );
+    });
+  }
+  return result;
+};
+
+const showPublicRoute = (routes) => {
+  let result = null;
+  if (routes.length > 0) {
+    result = routes.map((route, index) => {
+      return (
+        <Route
+          key={index}
+          path={route.path}
+          name={route.name}
+          exact={route.exact}
+          component={route.component}
+        />
+      );
+    });
+  }
+  return result;
+};
+
+function Layout(props) {
+  const { auth } = props;
+  return (
+    <>
+      {auth && <Header />}
+      <main>
+        <Switch>
+          {showPublicRoute(AUTH_ROUTES)}
+          {showPrivateRoute(HOME_ROUTES)}
+        </Switch>
+      </main>
+      <Footer />
+      <Loading />
+    </>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth.auth,
+  };
+};
+
+export default connect(mapStateToProps, null)(Layout);
